4b24233ffe98479f9524c6c5d464962e7fbdcda5
A-nish-kr <anish.12114317@lpu.in>
2024-11-23 22:41:24 +0530

    Added Components and routes

--- src/Components/FeaturedPosts.jsx
+++ src/Components/FeaturedPosts.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const PostListPage = () => {
+const FeaturedPosts = () => {
   return (
     <div>
+      
     </div>
   )
 }
 
-export default PostListPage
+export default FeaturedPosts
