93e86bdcf348ece474c2a98bbd5f72522285e501
A-nish-kr <anish.12114317@lpu.in>
2024-11-28 22:42:46 +0530

    Fixing routing

--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -7,15 +7,14 @@ import {
   uploadAuth,
   featurePost,
 } from "../controllers/post.controller.js";
-import increaseVisit from "../middleware/increaseVisit.js";
+import increaseVisit from "../middlewares/increaseVisit.js";
 
 const router = express.Router();
 
 router.get("/upload-auth", uploadAuth);
-
 router.get("/", getPosts);
 router.get("/:slug", increaseVisit, getPost);
-router.post("/", createPost);
+router.post("/create", createPost);
 router.delete("/:id", deletePost);
 router.patch("/feature", featurePost);
 
