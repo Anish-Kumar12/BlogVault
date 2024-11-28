25ffac6d3a9b500e137be3d930acfd0811aa96c3
A-nish-kr <anish.12114317@lpu.in>
2024-11-29 02:37:33 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // Middleware
 app.use(clerkMiddleware());
